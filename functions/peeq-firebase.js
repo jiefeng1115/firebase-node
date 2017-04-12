0cf1f0466c839209cc27b9a33c6a26cd7dfcd7ce
singyiu <sing@peeqdata.com>
2017-04-12 11:31:53 -0700

    adding src files

---
+++ functions/peeq-firebase.js
@@ -0,0 +1,69 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+var GeoPoint = require('geopoint');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://peeq-b81e7.firebaseio.com"
+});
+
+exports.admin = admin;
+
+//return a promise of snapshot? of the input refPath
+exports.snapshotOf = function(refPath) {
+  var database = admin.database();
+  return database.ref(refPath).once("value").then(function(snapshot) {
+    return new Promise((resolve, reject) => {
+      snapshot.exists() ? resolve(snapshot) : resolve(null);
+    });
+  });
+};
+
+//return a promise of firstChild snapshot? of the input snapshot
+exports.firstChildOf = function(snapshot) {
+  return new Promise((resolve, reject) => {
+    if (snapshot.hasChildren()) {
+      var childKey;
+      snapshot.forEach(function(childSnapshot) {
+          childKey = childSnapshot.key;
+          return true;    //to stop the enumeration after the first child
+      });
+      resolve(snapshot.child(childKey));
+    }
+    else {
+      resolve(null);
+    }
+  });
+};
+
+//return bool
+exports.isNearByGeoPoint = function(val1, val2) {
+  val1.geoPoint = new GeoPoint(val1.location.latitude, val1.location.longitude);
+  val2.geoPoint = new GeoPoint(val2.location.latitude, val2.location.longitude);
+  var distance = val1.geoPoint.distanceTo(val2.geoPoint); //miles
+  return (distance <= 1.0);
+};
+
+//return bool, for localSession vs localSession, and localSession vs playerHighlight
+exports.isRelated = function(snapshot1, snapshot2) {
+  if ((snapshot1.exists) && (snapshot2.exists)) {
+    val1 = snapshot1.val();
+    val2 = snapshot2.val();
+    return ((val1.user == val2.user) && (val1.channel == val2.channel) && (exports.isNearByGeoPoint(val1,val2)));
+  }
+  return false;
+};
+
+exports.snapshotsToArray = function(snapshots, filterFunction) {
+    var output = [];
+
+    snapshots.forEach(function(childSnapshot) {
+      output.push(childSnapshot);
+    });
+
+    if (filterFunction) {
+      output.filter(filterFunction);
+    }
+
+    return output;
+};
---
+++ functions/peeq-playerhighlight.js
@@ -0,0 +1,218 @@
+var peeqFirebase = require("./peeq-firebase");
+var admin = peeqFirebase.admin;
+
+var peeqDate = require("./peeq-date");
+var peeqSensorRecord = require("./peeq-sensorrecord");
+var firstBy = require('thenby');
+var peeqPlayerHighlightVideo = require("./peeq-playerhighlightvideo");
+var peeqVideoClip = require("./peeq-videoclip");
+
+const highlightedPDateOffsetStart = -1000 * 15; //TODO: Make this dynamic depends on the tracker data
+const highlightedPDateOffsetEnd = 1000 * 5;
+const generateHighlightDefaultNumOfVideos = 5; //TODO: Make this dynamic depends on the tracker data
+
+//PlayerHighlight class
+exports.PlayerHighlight = function PlayerHighlight(id, snapshot) {
+    this.id = id;
+    if (snapshot) {
+        this.snapshot = snapshot;
+    }
+
+    //return a promise of the original obj, with snapshot assigned to obj.snapshot + obj.val
+    this.fetchSnapshot = function() {
+        var obj = this;
+        return peeqFirebase.snapshotOf("playerHighlights/" + obj.id).then(function(snapshot) {
+            if ((snapshot) && (snapshot.exists())) {
+                obj.snapshot = snapshot;
+                obj.val = snapshot.val();
+                return Promise.resolve(obj);
+            } else {
+                return Promise.reject("invalid snapshot" + obj.id);
+            }
+        });
+    };
+
+    //return a promise of the original obj, with snapshot assigned to obj.snapshot
+    this.fetchSnapshotIfNeeded = function() {
+        return (this.snapshot ? Promise.resolve(this) : this.fetchSnapshot());
+    };
+
+
+    //return a promise of the original obj, with RelatedLocalSessionSnapshots assigned to obj.relatedLocalSessionSnapshots
+    this.fetchRelatedLocalSessionSnapshots = function() {
+        console.log("fetchRelatedLocalSessionSnapshots PlayerHighlight", this.id);
+        return this.fetchSnapshotIfNeeded().then(function(obj) {
+
+            var val = obj.val;
+            //console.log("obj.val", val);
+
+            var db = admin.database();
+            var ref = db.ref("localSessions");
+
+            var highlightedPDate = new peeqDate.PDate(val.timestamp);
+
+            var startAtStr = val.timestamp;
+            var endAtStr = highlightedPDate.dateStrWithTimeOffset(3600000 * 6);
+            //console.log("endAtStr", endAtStr);
+
+            return ref.orderByChild("endDate").startAt(startAtStr).endAt(endAtStr).once("value").then(function(filteredSnapshots) {
+
+                var relatedSnapshots = [];
+                filteredSnapshots.forEach(function(childSnapshot) {
+                    //console.log("childSnapshot", childSnapshot.val());
+                    if ((childSnapshot.val().startDate < val.timestamp) && (peeqFirebase.isRelated(obj.snapshot, childSnapshot))) {
+                        relatedSnapshots.push(childSnapshot);
+                        //console.log("relatedSnapshots", relatedSnapshots);
+                    }
+                });
+                obj.relatedLocalSessionSnapshots = relatedSnapshots;
+                //console.log("obj.relatedLocalSessionSnapshots", obj.relatedLocalSessionSnapshots);
+
+                return Promise.resolve(obj);
+            });
+        });
+    }; //End of fetchRelatedLocalSessionSnapshots
+
+
+    //return a promise of overallStat of the localSession in the specific time window
+    this.fetchTrackerStatisticOfLocalSessionInTimeWindow = function(localSessionSnapshot, startPDate, endPDate) {
+        var obj = this;
+        var localSessionId = localSessionSnapshot.key;
+        console.log("fetchTrackerStatisticOfLocalSessionInTimeWindow localSessionId", localSessionId);
+
+        var db = admin.database();
+        var ref = db.ref("sensorRecords/" + localSessionId);
+
+        var trackerStat = {};
+
+        return ref.orderByChild("timestamp").startAt(startPDate.dateStr).endAt(endPDate.dateStr).once("value").then(function(filteredSnapshots) {
+            //console.log("sensorRecords filteredSnapshots", filteredSnapshots.val());
+
+            if (!trackerStat[obj.id]) {
+                trackerStat[obj.id] = {};
+            }
+
+            var snapshotsArray = peeqFirebase.snapshotsToArray(filteredSnapshots);
+            var targetPlayerIds = Object.keys(obj.val.players);
+            console.log("filteredSnapshots", filteredSnapshots.numChildren(), "targetPlayerIds", targetPlayerIds);
+
+            var statisticArray = []; //statistic array holder for generating the overall statistic
+
+            targetPlayerIds.forEach(function(targetPlayerId) {
+                console.log("targetPlayerId", targetPlayerId);
+                var filteredArray = snapshotsArray.filter(function(element) {
+                    var playerId = element.val().player;
+                    return (playerId == targetPlayerId);
+                });
+
+                if (filteredArray.length > 0) {
+                    //console.log("filteredArray", filteredArray.length);
+                    var statistic = peeqSensorRecord.calculateStatisticFromSnapshotArray(filteredArray);
+                    //console.log("statistic", statistic);
+                    statisticArray.push(statistic);
+                    trackerStat[obj.id][targetPlayerId] = statistic;
+                }
+            });
+
+            var overallStat = peeqSensorRecord.calculateOverallStatisticFromStatisticArray(statisticArray);
+            overallStat.localSession = localSessionId;
+            overallStat.highlightStartTime = startPDate.timeInterval; //TODO: make the highlightStartTime and highlightEndTime dynamic
+            overallStat.highlightEndTime = endPDate.timeInterval;
+
+            //save trackerStat to firebase
+            if (overallStat.count > 0) {
+                var statRef = db.ref("trackerStatistics/");
+                statRef.set(trackerStat, function(error) {
+                    if (error) {
+                        console.error("Data could not be saved." + error);
+                    }
+                    //else {
+                    //  console.log("Data saved successfully.");
+                    //}
+                });
+                console.log("trackerStat", trackerStat);
+            }
+
+            return Promise.resolve(overallStat); //return overallStat as promise
+        });
+    }; //End of fetchTrackerStatisticOfLocalSessionInTimeWindow
+
+
+    //return all promise of each fetchTrackerStatisticOfLocalSessionInTimeWindow
+    this.generateTrackerStatisticIfNeeded = function() {
+        var obj = this;
+        console.log("generateTrackerStatisticIfNeeded", obj.id);
+
+        var highlightedPDate = new peeqDate.PDate(obj.val.timestamp);
+        var startPDate = highlightedPDate.PDateWithTimeOffset(highlightedPDateOffsetStart); //10sec
+        var endPDate = highlightedPDate.PDateWithTimeOffset(highlightedPDateOffsetEnd); //5sec
+
+        console.log("time window", startPDate.dateStr, endPDate.dateStr);
+
+        var promises = [];
+        obj.relatedLocalSessionSnapshots.forEach(function(localSessionSnapshot) {
+            //console.log("localSessionSnapshot",localSessionSnapshot.key, localSessionSnapshot.val());
+            var prom = obj.fetchTrackerStatisticOfLocalSessionInTimeWindow(localSessionSnapshot, startPDate, endPDate);
+            promises.push(prom);
+        });
+        return Promise.all(promises);
+    }; //end of generateTrackerStatisticIfNeeded
+
+
+    //return a promise of the playerHighlightVideo url
+    this.generateHighlightWithStatistics = function(stats) {
+        var obj = this;
+
+        if (stats.length > 0) {
+            var videoClips = stats.map(function(value) {
+                var videoClip = {};
+                videoClip.localSession = value.localSession;
+                videoClip.startTime = value.highlightStartTime;
+                videoClip.endTime = value.highlightEndTime;
+                return videoClip;
+            });
+            console.log("videoClips", videoClips);
+
+            return peeqVideoClip.createObjsInFirebaseIfNeeded(videoClips).then(function(videoClipResults) {
+                var videoClipIds = videoClipResults.map(function(value) {
+                    return value.id;
+                });
+
+                return peeqPlayerHighlightVideo.createObjInFirebaseIfNeeded(obj.val.user, obj.id, videoClipIds).then(function(playerHighlightVideoResult) {
+                    if (!playerHighlightVideoResult.isNew) {
+                        return Promise.reject("playerHighlightVideo already exist " + playerHighlightVideoResult.id);
+                    } else {
+                        var playerHighlightVideo = new peeqPlayerHighlightVideo.PlayerHighlightVideo(playerHighlightVideoResult.id);
+                        return playerHighlightVideo.generateVideo();
+                    }
+                });
+            });
+        }
+        return Promise.reject("empty stats");
+
+        //return obj.generateHighlightWithPlayerHighlightVideoSnapshot(obj.playerHighlightVideoSnapshot);
+    }; //end of generateHighlightWithStatistics
+
+
+    //return a promise of the playerHighlightVideo url
+    this.generateHighlightIfNeeded = function() {
+        console.log("generateHighlightIfNeeded", this.id);
+        return this.fetchRelatedLocalSessionSnapshots().then(function(obj) {
+            return obj.generateTrackerStatisticIfNeeded().then(function(overallStats) {
+                console.log("overallStats", overallStats);
+
+                var overallStatsSorted = overallStats.filter(function(value) { return (value.count > 0); })
+                    .sort(
+                        firstBy(function(v1, v2) { return Math.abs(v1.min) - Math.abs(v2.min); }) //proximity
+                        .thenBy(function(v1, v2) { return v2.numOfPlayers - v1.numOfPlayers; }) //num of highlighted players detected
+                        .thenBy(function(v1, v2) { return v2.duration - v1.duration; }) //coverage
+                    );
+                console.log("overallStatsSorted", overallStatsSorted);
+
+                var selectedStats = overallStatsSorted.slice(0, generateHighlightDefaultNumOfVideos);
+                return obj.generateHighlightWithStatistics(selectedStats);
+            });
+        });
+    }; //end of generateHighlightIfNeeded
+
+}; //End of exports.PlayerHighlight
