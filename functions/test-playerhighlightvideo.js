0e088c5c1cc0904fdbc9e0806b244a20e8cd0cd2
singyiu <sing@peeqdata.com>
2017-05-08 14:30:46 -0700

    v0.0.1b1

--- functions/test-playerhighlightvideo.js
+++ functions/test-playerhighlightvideo.js
@@ -1,5 +1,6 @@
 /*jshint esversion: 6 */
-
+var peeqFirebase = require("./peeq-firebase");
+var admin = peeqFirebase.admin;
 var peeqPlayerHighlightVideo = require("./peeq-playerhighlightvideo");
 
 if (process.argv[2] == "createObjsInFirebaseIfNeededWithInfo") {
@@ -22,6 +23,24 @@ if (process.argv[2] == "createObjsInFirebaseIfNeededWithInfo") {
 
     var playerHighlightVideo = new peeqPlayerHighlightVideo.PlayerHighlightVideo(playerHighlightVideoId);
     playerHighlightVideo.publishGenerateVideoMessage().then(result => {
+        console.log("result", result);
+    }).catch(err => {
+        console.error(err);
+    });
+} else if (process.argv[2] == "createPlayerHighlightVideoRecord") {
+    var targetId = "charityShield2017G1H18";
+    console.log("targetId", targetId);
+    var playerHighlightVideoInfo = {};
+    playerHighlightVideoInfo.localSessions = ["-KituJUR6k4g1nhWUGi_", "-KitwHH9MM2rpo59hwfo"];
+    playerHighlightVideoInfo.offsetStart = -3000;
+    playerHighlightVideoInfo.offsetEnd = 5000;
+    playerHighlightVideoInfo.playerHighlight = targetId;
+    playerHighlightVideoInfo.timestamp = "2017-04-29T13:51:25.009Z";
+    playerHighlightVideoInfo.user = "zHYwUQUyddOy0aplLSmQNnvtSjx1";
+
+    var db = admin.database();
+    var ref = db.ref("playerHighlightVideos");
+    ref.child(targetId).update(playerHighlightVideoInfo).then(result => {
         console.log("result", result);
     }).catch(err => {
         console.error(err);
